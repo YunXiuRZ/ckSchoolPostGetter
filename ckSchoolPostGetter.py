cd7c6dafc5c35d34677db8240a8538e78dbe38df
YunXiuRZ <70698588+YunXiuRZ@users.noreply.github.com>
2021-07-24 22:16:47 +0800

    add process message

--- ckSchoolPostGetter.py
+++ ckSchoolPostGetter.py
@@ -9,6 +9,7 @@ from PostInformationGetter import PostInformationGetter
 from MysqlPostConnector import MysqlPostConnector
 
 def get_post():
+    print("獲取近期公告")
     postUrl = "https://www2.ck.tp.edu.tw/news?page=1"
     r = requests.get(postUrl)
     r.encoding = 'utf8'
@@ -22,12 +23,15 @@ def get_post():
         #取最後五位數組爲ID
         Id = re.search(r'[0-9]{5}$', post.get("href")).group()
         idList.append(Id)
+        print(Id, end=' ')
+    print('')
 
     #檢查該ID是否已存在在資料庫
     for Id in idList:
-        print(Id)
+        print("取得編號%s公告內容" % Id)
         pig = PostInformationGetter(Id)
         pig.setInformation()
+        print("處理編號%s公告內容" % Id)
         mpc = MysqlPostConnector()
         mpc.execute(pig)
    
